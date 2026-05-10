2e5f402c7c166e07df80b2cf5a262a09be58868d
awcislo <ariel.wcislo@gmail.com>
2026-05-10 12:28:38 +0200

    Update galerry, remove dark mode, update settings

--- galeria.js
+++ galeria.js
@@ -3,17 +3,15 @@
 // Plik wrzuć do folderu assets/.
 
 const GALERIA = [
-  { src: './assets/01.jpg', caption: 'Dreamy Cookies - truskawka' },
-  { src: './assets/02.jpg', caption: 'Dreamy Cookies - lotus' },
-  { src: './assets/03.jpg', caption: 'Dreamy Cookies - czarna porzeczka' },
-  { src: './assets/04.jpg', caption: 'Sernik baskijski' },
-  { src: './assets/05.jpg', caption: 'Cynamonki z kremem śmietankowym' },
-  { src: './assets/06.jpg', caption: 'Monkey bread z kremem korzennym' },
-  { src: './assets/07.jpg', caption: 'Monkey bread z kremem korzennym' },
-  { src: './assets/08.jpg', caption: 'Monkey bread z kremem korzennym' },
-  { src: './assets/09.jpg', caption: 'Monkey bread z kremem korzennym' },
-  { src: './assets/10.jpg', caption: 'Monkey bread z kremem korzennym' },
-  { src: './assets/11.jpg', caption: 'Monkey bread z kremem korzennym' },
-  { src: './assets/12.jpg', caption: 'Monkey bread z kremem korzennym' },
-  // { src: './assets/zdj7.jpg', caption: 'Opis zdjęcia' },
+  { src: './assets/01.png', caption: '' },
+  { src: './assets/02.png', caption: '' },
+  { src: './assets/03.png', caption: '' },
+  { src: './assets/04.png', caption: '' },
+  { src: './assets/05.png', caption: '' },
+  { src: './assets/06.png', caption: '' },
+  { src: './assets/07.png', caption: '' },
+  { src: './assets/08.png', caption: '' },
+  { src: './assets/09.png', caption: '' },
+  { src: './assets/10.png', caption: '' },
+  // { src: './assets/zdj7.png', caption: 'Opis zdjęcia' },
 ];
